2a381f175bc9f3854de9373030479c86d604a435
FernandoCerqEng19 <fernando11cerqueira@gmail.com>
2023-03-28 21:47:04 -0300

    Add Ex07

---
+++ Lista1_08.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int a,b,c,d;
+    int diferenca;
+
+    printf("\nInsira o valor de A (deve ser inteiro): ");
+    scanf("%d", &a);
+    printf("\nInsira o valor de B (deve ser inteiro): ");
+    scanf("%d", &b);
+    printf("\nInsira o valor de C (deve ser inteiro): ");
+    scanf("%d", &c);
+    printf("\nInsira o valor de D (deve ser inteiro): ");
+    scanf("%d", &d);
+
+    diferenca = ((a * b) - (c * d));
+    
+    printf("\nDiferenca de: %d x %d - %d x %d",a,b,c,d);
+    printf("\nDiferenca = %d", diferenca);
+}
