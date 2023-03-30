93bb705da7e489f2e004b9d10c75354f2604b165
FernandoCerqEng19 <fernando11cerqueira@gmail.com>
2023-03-29 21:01:43 -0300

    Add Ex03

---
+++ Lista2_03.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int numero;
+
+    printf("Insira um número: ");
+    scanf("%d", &numero);
+
+    if (){
+        printf("\nO numero informado e PAR!");
+    }
+    else{
+        printf("\nO numero informado e IMPAR");
+    }
+}
