32907103052db87b3cb5cf26ada1e765f2724d84
FernandoCerqEng19 <fernando11cerqueira@gmail.com>
2023-05-22 19:56:06 -0300

    add 2

--- PROJETO VETOR E FUNÇÃO/Projeto.c
+++ PROJETO VETOR E FUNÇÃO/Projeto.c
@@ -29,3 +29,36 @@ void imprimirUsuario(int index) {
     printf("Vacina: %s\n\n", (vacinas[index] == 1) ? "Sim" : "Não");
 }
 
+void incluirUsuario() {
+    int id = gerarNumeroAleatorio(1000, 9999);
+    ids[numUsuarios] = id;
+
+    printf("Digite o nome completo: ");
+    fgets(nomes[numUsuarios], MAX_STRING_LENGTH, stdin);
+    nomes[numUsuarios][strcspn(nomes[numUsuarios], "\n")] = '\0'; // Remove o caractere de nova linha
+
+    printf("Digite o email: ");
+    fgets(emails[numUsuarios], MAX_STRING_LENGTH, stdin);
+    emails[numUsuarios][strcspn(emails[numUsuarios], "\n")] = '\0';
+
+    printf("Digite o sexo (Feminino, Masculino, Outro): ");
+    fgets(sexos[numUsuarios], MAX_STRING_LENGTH, stdin);
+    sexos[numUsuarios][strcspn(sexos[numUsuarios], "\n")] = '\0';
+
+    printf("Digite o endereco: ");
+    fgets(enderecos[numUsuarios], MAX_STRING_LENGTH, stdin);
+    enderecos[numUsuarios][strcspn(enderecos[numUsuarios], "\n")] = '\0';
+
+    printf("Digite a altura (entre 1 e 2 metros): ");
+    scanf("%lf", &alturas[numUsuarios]);
+    getchar();
+
+    printf("Recebeu a vacina? (1 para sim, 0 para não): ");
+    scanf("%d", &vacinas[numUsuarios]);
+    getchar();
+
+    numUsuarios++;
+
+    printf("Usuario cadastrado com sucesso!\n\n");
+}
+
